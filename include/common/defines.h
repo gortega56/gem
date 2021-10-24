d1c37c34943aa6e453927166f23718a7afdbda2a
Gabriel Ortega <gabriel.ortega56@gmail.com>
2021-10-24 13:48:59 -0400

    Add macro option for vectorcall

--- include/common/defines.h
+++ include/common/defines.h
@@ -1,8 +1,17 @@
 #pragma once
+
 #if defined(_MSC_VER)
 #define GEM_INLINE __forceinline
-#define GEM_VECTORCALL __vectorcall
 #else
 #define GEM_INLINE inline
+#endif
+
+#ifdef GEM_VECTORCALL_ENABLE
+#if defined(_MSC_VER)
+#define GEM_VECTORCALL __vectorcall
+#else
+#define GEM_VECTORCALL
+#endif
+#else
 #define GEM_VECTORCALL
 #endif
